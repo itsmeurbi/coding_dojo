83d4c52ccddbf0e105d5371752dadd364ffcffe8
Miguel Urbina <itismeurbi@gmail.com>
2022-07-18 14:44:46 -0500

    Add game 1 missing variables operators spacing

--- tennis_refactoring/tennis_game1.rb
+++ tennis_refactoring/tennis_game1.rb
@@ -16,30 +16,30 @@ class TennisGame1
 
   def score
     result = ""
-    temp_score=0
-    if (@p1_points==@p2_points)
+    temp_score = 0
+    if (@p1_points == @p2_points)
       result = {
           0 => "Love-All",
           1 => "Fifteen-All",
           2 => "Thirty-All",
       }.fetch(@p1_points, "Deuce")
-    elsif (@p1_points>=4 or @p2_points>=4)
-      minus_result = @p1_points-@p2_points
-      if (minus_result==1)
-        result ="Advantage player1"
-      elsif (minus_result ==-1)
-        result ="Advantage player2"
-      elsif (minus_result>=2)
+    elsif (@p1_points >= 4 or @p2_points >= 4)
+      minus_result = @p1_points - @p2_points
+      if (minus_result == 1)
+        result = "Advantage player1"
+      elsif (minus_result ==- 1)
+        result = "Advantage player2"
+      elsif (minus_result >= 2)
         result = "Win for player1"
       else
-        result ="Win for player2"
+        result = "Win for player2"
       end
     else
       (1...3).each do |i|
-        if (i==1)
+        if (i == 1)
           temp_score = @p1_points
         else
-          result+="-"
+          result += "-"
           temp_score = @p2_points
         end
         result += {
