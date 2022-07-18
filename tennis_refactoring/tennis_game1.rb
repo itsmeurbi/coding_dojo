34566644731ba9e03264ba3b08166a092261832f
Miguel Urbina <itismeurbi@gmail.com>
2022-07-18 14:54:24 -0500

    Use single quote on game 1

--- tennis_refactoring/tennis_game1.rb
+++ tennis_refactoring/tennis_game1.rb
@@ -7,7 +7,7 @@ class TennisGame1
   end
 
   def won_point(player_name)
-    if player_name == "player1"
+    if player_name == 'player1'
       @p1_points += 1
     else
       @p2_points += 1
@@ -15,38 +15,38 @@ class TennisGame1
   end
 
   def score
-    result = ""
+    result = ''
     temp_score = 0
     if (@p1_points == @p2_points)
       result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
-      }.fetch(@p1_points, "Deuce")
+          0 => 'Love-All',
+          1 => 'Fifteen-All',
+          2 => 'Thirty-All',
+      }.fetch(@p1_points, 'Deuce')
     elsif (@p1_points >= 4 or @p2_points >= 4)
       minus_result = @p1_points - @p2_points
       if (minus_result == 1)
-        result = "Advantage player1"
+        result = 'Advantage player1'
       elsif (minus_result ==- 1)
-        result = "Advantage player2"
+        result = 'Advantage player2'
       elsif (minus_result >= 2)
-        result = "Win for player1"
+        result = 'Win for player1'
       else
-        result = "Win for player2"
+        result = 'Win for player2'
       end
     else
       (1...3).each do |i|
         if (i == 1)
           temp_score = @p1_points
         else
-          result += "-"
+          result += '-'
           temp_score = @p2_points
         end
         result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
+            0 => 'Love',
+            1 => 'Fifteen',
+            2 => 'Thirty',
+            3 => 'Forty',
         }[temp_score]
       end
     end
