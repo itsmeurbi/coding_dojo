6fc33abe66d13c3eb2093b1344adcccc7e73b53c
Miguel Urbina <itismeurbi@gmail.com>
2022-07-26 10:11:41 -0500

    Increate number of tests

--- dot_calculator/dot_calculator_test.rb
+++ dot_calculator/dot_calculator_test.rb
@@ -1,29 +1,59 @@
 require 'minitest'
-require 'minitest/autorun'
-require_relative 'dot_calculator'
+ require 'minitest/autorun'
+ require_relative 'dot_calculator'
 
-class DotCalculatorTest < Minitest::Test
-  def test_addition_works
-    response = dot_calculator('. + .')
+ class DotCalculatorTest < Minitest::Test
+   def test_addition_works1
+     response = dot_calculator('. + .')
 
-    assert_equal('..', response)
-  end
+     assert_equal('..', response)
+   end
 
-  def test_substraction_works
-    response = dot_calculator('..... - ..')
+   def test_addition_works2
+     response = dot_calculator('... + .')
 
-    assert_equal('...', response)
-  end
+     assert_equal('....', response)
+   end
 
-  def test_multiplication_works
-    response = dot_calculator('.. * ........')
+   def test_substraction_works1
+     response = dot_calculator('..... - ..')
 
-    assert_equal('................', response)
-  end
+     assert_equal('...', response)
+   end
 
-  def test_integer_division_works
-    response = dot_calculator('.... // ..')
+   def test_substraction_works2
+     response = dot_calculator('.. - .')
 
-    assert_equal('..', response)
-  end
-end
+     assert_equal('.', response)
+   end
+
+   def test_multiplication_works1
+     response = dot_calculator('.. * ........')
+
+     assert_equal('................', response)
+   end
+
+   def test_multiplication_works2
+     response = dot_calculator('.. * ..')
+
+     assert_equal('....', response)
+   end
+
+   def test_integer_division_works
+     response = dot_calculator('.... // ..')
+
+     assert_equal('..', response)
+   end
+
+   def test_integer_division_works1
+     response = dot_calculator('.... // ..')
+
+     assert_equal('..', response)
+   end
+
+   def test_integer_division_works2
+     response = dot_calculator('.. // ...')
+
+     assert_equal('', response)
+   end
+ end
