44ac3725f7372274fe751f7f72a01999463b3217
Miguel Ángel Urbina <itismeurbi@gmail.com>
2022-07-26 10:14:51 -0500

    Dot calculator update (#3)
    * Increate number of tests
    
    * Refactor dot calculator

--- dot_calculator/dot_calculator_test.rb
+++ dot_calculator/dot_calculator_test.rb
@@ -3,27 +3,57 @@ require 'minitest/autorun'
 require_relative 'dot_calculator'
 
 class DotCalculatorTest < Minitest::Test
-  def test_addition_works
+  def test_addition_works1
     response = dot_calculator('. + .')
 
     assert_equal('..', response)
   end
 
-  def test_substraction_works
+  def test_addition_works2
+    response = dot_calculator('... + .')
+
+    assert_equal('....', response)
+  end
+
+  def test_substraction_works1
     response = dot_calculator('..... - ..')
 
     assert_equal('...', response)
   end
 
-  def test_multiplication_works
+  def test_substraction_works2
+    response = dot_calculator('.. - .')
+
+    assert_equal('.', response)
+  end
+
+  def test_multiplication_works1
     response = dot_calculator('.. * ........')
 
     assert_equal('................', response)
+  end
+
+  def test_multiplication_works2
+    response = dot_calculator('.. * ..')
+
+    assert_equal('....', response)
   end
 
   def test_integer_division_works
     response = dot_calculator('.... // ..')
 
     assert_equal('..', response)
+  end
+
+  def test_integer_division_works1
+    response = dot_calculator('.... // ..')
+
+    assert_equal('..', response)
+  end
+
+  def test_integer_division_works2
+    response = dot_calculator('.. // ...')
+
+    assert_equal('', response)
   end
 end
