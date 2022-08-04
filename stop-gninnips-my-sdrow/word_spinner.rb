259caa5d38e3613d654a7f9ef5fd4c4c1ed17759
Miguel Urbina <itismeurbi@gmail.com>
2022-08-04 16:08:28 -0500

    Define max number of characters per reversable word const

--- stop-gninnips-my-sdrow/word_spinner.rb
+++ stop-gninnips-my-sdrow/word_spinner.rb
@@ -1,7 +1,10 @@
+SIZE_FOR_WORD_TO_BE_REVERSED = 5
+
+# O(n)
 def word_spinner(string_input)
   words = string_input.split(' ')
   words.map do |word|
-    next word if word.size < 5
+    next word if word.size < SIZE_FOR_WORD_TO_BE_REVERSED
 
     word.reverse
   end.join(' ')
