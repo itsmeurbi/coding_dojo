08637d225ff61b7868e11c45af0bbd4ec0567aa2
Miguel Urbina <itismeurbi@gmail.com>
2022-08-15 19:13:54 -0500

    Make ids extracter method recursive

--- extract_ids_from_dataset/id_extracter.rb
+++ extract_ids_from_dataset/id_extracter.rb
@@ -1,19 +1,11 @@
-def extract_ids(structure)
-  return [] unless structure[:id].present?
+def extract_ids(structure, ids = [])
+  return ids unless structure[:id].present?
 
-  ids = []
   ids << structure[:id]
   if structure[:items].present?
-    process_items(structure, ids)
-  end
-  ids
-end
-
-def process_items(structure, ids)
-  structure[:items].each do |struc|
-    ids << struc[:id]
-    if struc[:items].present?
-      process_items(struc, ids)
+    structure[:items].each do |struc|
+      extract_ids(struc, ids)
     end
   end
+  ids
 end
